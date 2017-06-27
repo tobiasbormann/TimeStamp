1a437fabe27b823be5c68ab2e1818f499275ccad
tobiasbormann <info@tobiasbormann.de>
2017-06-27 21:06:50 +0200

    versionnr lowered

--- main_application/src/main/Main_Application.java
+++ main_application/src/main/Main_Application.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 
 public class Main_Application extends Application {
 
-    public static final String build = "0.3";
+    public static final String build = "0.25";
 
     private final String fn = "SourceSansPro-";
     private final String[] fonts = {"uiicons.ttf", fn+"Black.tff", fn+"BlackItalic.tff", fn+"Bold.tff",
